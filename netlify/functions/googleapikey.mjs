c35e4a897430c0fdf5e4f4c2af3841dd9a95de3f
fauxserious <fauxserious@gmail.com>
2024-08-05 01:15:33 +0000

    Cleanup

--- netlify/functions/googleapikey.mjs
+++
@@ -1,8 +0,0 @@
-exports.handler = async function (event, context) {
-    const apiKey = process.env.GOOGLE_API_KEY;
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: apiKey }),
-    };
-};
