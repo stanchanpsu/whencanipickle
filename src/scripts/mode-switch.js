a09c518525048ccf5dc13d188ed89fa7efb581c8
fauxserious <fauxserious@gmail.com>
2024-08-19 01:50:19 +0000

    Save state

--- src/scripts/mode-switch.js
+++ src/scripts/mode-switch.js
@@ -2,22 +2,34 @@
 const MODE_LOCALSTORAGE_KEY = 'mode';
 const $button = document.getElementById('mode-switch');
 
-let mode = localStorage.getItem(MODE_LOCALSTORAGE_KEY);
-
-function update() {
+/**
+ * Updates the UI to the new mode.
+ * 
+ * @param {String} mode - New mode to apply.
+ */
+function update(mode) {
     $button.dataset.mode = mode;
     document.body.style.setProperty('color-scheme', mode);
 }
 
+/**
+ * Initializes the mode switch.
+ * Determines how to set based on saved setting,
+ * or OS preference.
+ * 
+ * @param {MediaEvent} ev - Media Event object 
+ */
 function init(ev) {
-    mode = ev.matches ? 'light' : 'dark';
-    update();
+    const storage = localStorage.getItem(MODE_LOCALSTORAGE_KEY);
+    if (typeof storage === 'string') return update(storage);
+    else if (ev.matches) return update('light');
+    else return update('dark');
 }
 
 $button.addEventListener('click', () => {
-    mode = ['light', 'dark'].find((m) => mode !== m);
-    update();
+    const mode = ['light', 'dark'].find((m) => $button.dataset.mode !== m);
+    localStorage.setItem(MODE_LOCALSTORAGE_KEY, mode);
+    update(mode);
 });
 
 init(window.matchMedia('(prefers-color-scheme: light)'));
-
